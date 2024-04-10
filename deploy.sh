432ea17dfb1c8a1e6550f506cbc112cf6c686233
VMois <vmois@protonmail.com>
2024-04-09 22:39:02 -0400

    Make deploy script check if .env exist, abort if not

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ ! -f .env ]; then
+    echo ".env is not found! Aborting!"
+    exit 1
+fi
+
 git pull origin main
 docker compose build
 docker compose down && docker compose up -d
