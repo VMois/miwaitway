1915dd280a08bcb659d33d185aff33bf117856b5
VMois <vmois@protonmail.com>
2024-05-08 20:35:50 -0400

    Add DAG to transform vehicle positions and stops from raw to prod

--- dags/config.py
+++ dags/config.py
@@ -1,8 +1,9 @@
-BUCKET_NAME = 'miwaitway'
-GCP_CONN_ID = 'miwaitway_gcp_default'
-PROJECT_ID ='miwaitway'
-RAW_DATASET_NAME = 'raw_miway_data'
+BUCKET_NAME = "miwaitway"
+GCP_CONN_ID = "miwaitway_gcp_default"
+PROJECT_ID = "miwaitway"
+RAW_DATASET_NAME = "raw_miway_data"
+PROD_DATASET_NAME = "prod_miway_data"
 
 # Vehicle position tables
-STAGE_VEHICLE_TABLE_NAME = 'stage_vehicle_position'
-VEHICLE_TABLE_NAME = 'vehicle_position'
+STAGE_VEHICLE_TABLE_NAME = "stage_vehicle_position"
+VEHICLE_TABLE_NAME = "vehicle_position"
