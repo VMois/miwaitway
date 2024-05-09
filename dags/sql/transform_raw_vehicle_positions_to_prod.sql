8e0cda4cad4c1fee98c0ccd5cfa41903f2e93a72
VMois <vmois@protonmail.com>
2024-05-08 21:17:43 -0400

    Fix SQL to transform vehicle positions from raw to prod in multiple places

--- dags/sql/transform_raw_vehicle_positions_to_prod.sql
+++ dags/sql/transform_raw_vehicle_positions_to_prod.sql
@@ -1,8 +1,8 @@
 DECLARE var_today STRING;
-SET var_today = "{{ ds }}";
 DECLARE var_yesterday STRING;
-SET var_yesterday = "{{ macros.ds_add(ds, -1) }}";
 DECLARE var_today_est_limit TIMESTAMP;
+SET var_today = "{{ ds }}";
+SET var_yesterday = "{{ macros.ds_add(ds, -1) }}";
 SET var_today_est_limit = TIMESTAMP("{{ ds }} 04:00:00 UTC");
 
 MERGE `{{ params.project_id }}.{{ params.prod_dataset_id }}.{{ params.vehicle_table_name }}` AS target
@@ -10,18 +10,18 @@ USING (
     SELECT * FROM `{{ params.project_id }}.{{ params.raw_dataset_id }}.{{ params.vehicle_table_name }}` 
     WHERE (TIMESTAMP_TRUNC(timestamp, DAY) = TIMESTAMP(var_today) 
           OR TIMESTAMP_TRUNC(timestamp, DAY) = TIMESTAMP(var_yesterday))
-          AND TIMESTAMP_TRUNC(timestamp, HOUR) <= var_today_est_limit)
+          AND TIMESTAMP_TRUNC(timestamp, HOUR) <= var_today_est_limit
 ) AS source
-    ON target.vehicle_id = source.vehicle_id
+    ON target.vehicle_id = CAST(source.vehicle_id AS INT64)
         AND target.timestamp = source.timestamp
 WHEN MATCHED THEN
     UPDATE SET
         target.id = source.id
-        , target.trip_id = source.trip_id
+        , target.trip_id = CAST(source.trip_id AS INT64)
         , target.route_id = source.route_id
         , target.direction_id = source.direction_id
         , target.start_date = source.start_date
-        , target.vehicle_id = source.vehicle_id
+        , target.vehicle_id = CAST(source.vehicle_id AS INT64)
         , target.vehicle_label = source.vehicle_label
         , target.location_point = ST_GeogPoint(source.longitude, source.latitude)
         , target.bearing = source.bearing
@@ -46,11 +46,11 @@ WHEN NOT MATCHED THEN
         , occupancy_percentage
     ) VALUES (
         source.id
-        , source.trip_id
+        , CAST(source.trip_id AS INT64)
         , source.route_id
         , source.direction_id
         , source.start_date
-        , source.vehicle_id
+        , CAST(source.vehicle_id AS INT64)
         , source.vehicle_label
         , ST_GeogPoint(source.longitude, source.latitude)
         , source.bearing
